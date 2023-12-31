7b575c9f0b5da22fdde2858145e69b19f99d19fa
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2023-12-31 19:21:50 +0000

    config fo js

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+console.log("Rahman");
+
+let age = 23;
+let isLoggedIn = false;
+let state;
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalonevalue
+// undefined
+// symbol => unique
+
+
+// object
