6041d88766d59ed0a2e78dc3198e00ac49cb2e2f
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2023-12-31 17:12:15 +0000

    config for js

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 1.444;
+let accountEmail = "rahman@gmail.com";
+var accountpassword  = "12345";
+
+
+
+// accountId = 2; not allowed
+accountEmail = "saniya@gmail.com";
+accountpassword = "RahmanKhan"
+
+
+/* 
+
+var not use 
+because issue is blcok scope and functional scope
+
+*/ 
+
+console.table([ accountId , accountEmail , accountpassword]);
+
