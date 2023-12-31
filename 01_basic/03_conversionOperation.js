a4dfefe6968a111361950eee2fa4f30114bad48d
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2023-12-31 19:21:09 +0000

    config for js

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+console.log(typeof (score));
+
+let isScoreNumber = Number(score)
+console.log (typeof isScoreNumber)
+console.log(isScoreNumber)
+
+
+//"99" => 99
+//"99xyz" => Nan(not a number)
+// true => 1 , false => 0
+
+let isLoggedIn = 0;
+
+let bolleanLoggedIn = Boolean(isLoggedIn);
+console.log(bolleanLoggedIn)
+
+
