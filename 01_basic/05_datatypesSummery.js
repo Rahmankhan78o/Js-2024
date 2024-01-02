a9d3a50f7eb600b892ee230c89a62fca0dd12530
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2024-01-02 05:18:25 +0000

    data type summery

---
+++ 01_basic/05_datatypesSummery.js
@@ -0,0 +1,35 @@
+// primitive datatype
+
+// string, Number , Boolean , null, undefined, symbol
+
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+
+let userEmail;
+
+const Id = symbol('123')
+
+const anotherId = symbol('123')
+
+console.log(Id === anotherId)
+
+// Reference Array, Object , Function 
+
+
+const hero = ["Rahman" , "khan" , "Imran Khan", "Farman" , "Saniya" ]
+
+const herosdata = {
+    name: "Rahman", 
+    age: 23,
+}
+
+const myFunction = function() {
+    console.log("Rahman Khan")
+}
+
+
+
