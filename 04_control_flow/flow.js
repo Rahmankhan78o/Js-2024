1074a418d42526c938ce88f16a251700a1b82a58
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2024-01-09 05:36:57 +0000

    control flow js

---
+++ 04_control_flow/flow.js
@@ -0,0 +1,16 @@
+const tempretur = 41;
+
+if(2 < 3) {
+    console.log("tempretur is very cool" )
+}
+else{
+    console.log("tempreture is very hot")
+}
+
+const score = 200
+
+if(score > 100) {
+    const power = "fly"
+    console.log(`rahman coder and , ${power}`)
+}
+// console.log(`user ${power}`) not required for const and let for block scope 
