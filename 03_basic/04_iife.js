b77ec388504d1c6b7f8cbf20b4c07b2387f484f0
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2024-01-09 04:39:26 +0000

    iife js

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression (IIFE)
+
+( function  rahmaniife() {
+    console.log("db connected")
+})();
+
+// second options
+
+( (name) => {
+    console.log(`${name} welcome in your company`)
+})("Rahman")
