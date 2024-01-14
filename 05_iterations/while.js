2146d5bef17c30a1eef32a4f2276985a011bfce3
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2024-01-14 07:09:44 +0000

    while do while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,27 @@
+
+
+let index = 1;
+
+while (index <= 10){
+    console.log(`value of index is ${index}`)
+    index = index * 2
+
+}
+
+let myArr = ["Bollywod" , "South" ,"Hollywod"]
+
+let arr = 0;
+while (arr < myArr.length) {
+
+    console.log(`value of arr is ${myArr[arr]}`)
+
+    arr = arr + 1
+    
+}
+
+let score = 10;
+
+do {
+    console.log(`Best Score is ${score}`);
+    score++
+} while(score <= 10)
