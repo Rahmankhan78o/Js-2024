a9c2b8badc65dbdc0e47ae1984e6d3d689c13ecb
Mohmmad Rahman <97583820+Rahmankhan78o@users.noreply.github.com>
2024-02-03 11:53:31 +0530

    fetch data

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -82,3 +82,25 @@ async function consumePromiseFive() {
   }
 }
 consumePromiseFive();
+
+async function getAllUsers(){
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log("error:" , error)
+  }
+}
+getAllUsers();
+
+  fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    console.log(data)
+  }).catch((error) => {
+    console.log(error)
+  })
+    
